a2e2e738d256fb046d4ccf25f95d241c09c4d9c5
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-19 18:54:45 +0300

    0.04 admin

--- parser/parserAvitoCian/admin.py
+++ parser/parserAvitoCian/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Advertisement, Source
+from django.utils.html import format_html
+
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['url', 'title', 'price', 'phone', 'appartment_square', 'appartment_floor', 'floors_count', 'marketing_source', 'date']
+    list_display = ['print_url_shortly', 'title', 'price', 'print_phone_as_link', 'appartment_square', 'appartment_floor', 'floors_count', 'marketing_source', 'date']
+
+    def print_url_shortly(self, object):
+        return object.url[20:][:30]
+
+    def print_phone_as_link(self, obj):
+        return format_html(f'<a href="{obj.phone}">{obj.phone}</a>')
+
 
 admin.site.register(Advertisement, AdvertisementAdmin)
 
