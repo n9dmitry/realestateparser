8da334d756f0b8c4d08e3b7e5c33bdf030505fd2
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-19 17:22:43 +0300

    0.03

--- parser/parserAvitoCian/admin.py
+++ parser/parserAvitoCian/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Advertisement, Source
 
-# Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['url', 'title', 'price', 'phone', 'appartment_square', 'appartment_floor', 'floors_count', 'marketing_source', 'date']
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+
+
+admin.site.register(Source)
--- parser/parserAvitoCian/models.py
+++ parser/parserAvitoCian/models.py
@@ -4,10 +4,15 @@ from django.db import models
 
 class Source(models.Model):
     title = models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.title}"
 
 class Advertisement(models.Model):
     date = models.DateTimeField()
+    phone = models.IntegerField(blank=True, null=True)
+    url = models.CharField(max_length=1000, blank=True, null=True)
     title = models.CharField(max_length=300)
+    price = models.IntegerField(blank=True, null=True)
     appartment_square = models.IntegerField()
     appartment_floor = models.IntegerField()
     floors_count = models.IntegerField()
