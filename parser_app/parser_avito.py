22158a7c02c6cb910c8596e86908392f439966e4
Trackktoor <pipleson212@gmail.com>
2023-02-23 14:52:21 +0500

    fix

--- parser_app/parser_avito.py
+++ parser_app/parser_avito.py
@@ -1,2 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+
+def parser_avito(url):
+    page = requests.get(url)
+    return page
---
+++ parser_app/parser_manager.py
@@ -0,0 +1,1 @@
+url = 'https://www.avito.ru/moskva/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?f=ASgBAgICA0SSA8gQ8AeQUsDBDbr9Nw&localPriority=0&s=104&user=1'
