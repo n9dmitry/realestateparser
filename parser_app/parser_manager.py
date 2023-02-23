87b988c59923121f96ee69e11a165a288f9d5b36
Trackktoor <pipleson212@gmail.com>
2023-02-23 15:17:41 +0500

    fix

--- parser_app/parser_manager.py
+++ parser_app/parser_manager.py
@@ -1,1 +1,5 @@
+from parser_avito import parser_avito
+
 url = 'https://www.avito.ru/moskva/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?f=ASgBAgICA0SSA8gQ8AeQUsDBDbr9Nw&localPriority=0&s=104&user=1'
+
+print(parser_avito(url))
