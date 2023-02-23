eddde634d8cfe2c9bbb8c23e418cee5ebc7352dd
Trackktoor <pipleson212@gmail.com>
2023-02-23 15:37:15 +0500

    fix

--- parser_app/parser_avito.py
+++ parser_app/parser_avito.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 
 def parser_avito(url):
-    page = requests.get(url)
+    headers = {
+        "Accept": "*/*",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3'
+    }
+
+    page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.text, 'lxml')
     return soup
