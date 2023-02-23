ff56a4b77e471f7427e0cb64c1fb5d9f00e9c303
Trackktoor <pipleson212@gmail.com>
2023-02-23 15:23:46 +0500

    fix

--- parser_app/parser_avito.py
+++ parser_app/parser_avito.py
@@ -4,5 +4,4 @@ import requests
 def parser_avito(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'lxml')
-    allNews = soup.find_all('div',{'class':'iva-item-root-_lk9K photo-slider-slider-S15A_ iva-item-list-rfgcH iva-item-redesign-rop6P iva-item-responsive-_lbhG items-item-My3ih items-listItem-Gd1jN js-catalog-item-enum'})
-    return allNews
+    return soup
