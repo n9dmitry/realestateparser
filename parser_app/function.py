41a88187b3f9f87b98d8036178520de455761550
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-26 16:07:40 +0300

    0.14

---
+++ parser_app/function.py
@@ -0,0 +1,13 @@
+import pytesseract
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+from pytesseract import image_to_string
+
+
+def convert_image_to_string(png):
+    image = Image.open(png)
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    string = pytesseract.image_to_string(image)
+    print(string)
+convert_image_to_string('media/test.png')
